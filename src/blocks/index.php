c0d66dea243a47f59bcb63ea271fa787d4661ae4
Tobias Bleckert <hola@tobiasbleckert.se>
2020-01-20 09:17:54 +0100

    Avoid duplicates

--- src/blocks/index.php
+++ src/blocks/index.php
@@ -1,22 +1,25 @@
 <?php
 
-/**
- * Applies namespace to classes.
- *
- * @param string $class One or multiple classes separated by spaces.
- *
- * @return void
- */
-function class_name( $class ) {
-	$namespace = apply_filters( 'iis_blocks_namespace', 'iis-' );
-	$classes   = array_map(
-		function ( $single_class ) use ( $namespace ) {
-			return $namespace . $single_class;
-		},
-		explode( ' ', $class )
-	);
+if ( ! function_exists( 'class_name' ) ) {
+	/**
+	 * Applies namespace to classes.
+	 *
+	 * @param string $class One or multiple classes separated by spaces.
+	 *
+	 * @return void
+	 */
+	function class_name($class)
+	{
+		$namespace = apply_filters( 'iis_blocks_namespace', 'iis-' );
+		$classes = array_map(
+			function ($single_class) use ($namespace) {
+				return $namespace . $single_class;
+			},
+			explode( ' ', $class )
+		);
 
-	echo esc_attr( implode( ' ', $classes ) );
+		echo esc_attr( implode( ' ', $classes ) );
+	}
 }
 
 require_once __DIR__ . '/download.php';
