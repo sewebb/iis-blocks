804cba5d8781be38b9e0794c43cec50c20b2ef57
emelie <emelie@earthpeople.se>
2020-05-20 18:38:03 +0200

    Set grid class based on column width attribute when rendering column block

--- src/blocks/column.php
+++ src/blocks/column.php
@@ -1,11 +1,16 @@
 <?php
 
 function iis_render_column( $_, $content ) {
+	$column_width 	= isset($_['columnWidth']) ? $_['columnWidth'] : '1/3';
+	$grid_class 	= $column_width === '1/3' ? 'grid-18 grid-md-6' : 'grid-18 grid-md-9';
+
 	ob_start();
 	?>
-	<div class="grid">
+
+	<div class="<?php echo $grid_class; ?>">
 		<?php echo $content; ?>
 	</div>
+
 	<?php
 
 	return ob_get_clean();
