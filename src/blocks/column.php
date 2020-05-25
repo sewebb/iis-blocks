24ce3b7ca85eeca1e527ad46ab42a988db105258
emelie <emelie@earthpeople.se>
2020-05-25 10:35:29 +0200

    Rename argument variable and add some bottom margin

--- src/blocks/column.php
+++ src/blocks/column.php
@@ -1,13 +1,25 @@
 <?php
 
-function iis_render_column( $_, $content ) {
-	$column_width 	= isset($_['columnWidth']) ? $_['columnWidth'] : '1/3';
-	$grid_class 	= $column_width === '1/3' ? 'grid-18 grid-md-6' : 'grid-18 grid-md-9';
+function iis_render_column( $attributes, $content ) {
+	$column_width 	= $attributes['columnWidth'] ?? '';
+
+	$grid_class = '';
+	switch ($column_width) {
+		case '1/2':
+			$grid_class = 'grid-18 grid-md-9';
+			break;
+	    case '1/3':
+	        $grid_class = 'grid-18 grid-md-6';
+	        break;
+		default:
+			$grid_class = 'grid-18';
+			break;
+	}
 
 	ob_start();
 	?>
 
-	<div class="<?php echo $grid_class; ?>">
+	<div class="<?php echo $grid_class; ?> u-m-b-4">
 		<?php echo $content; ?>
 	</div>
 
