710e8a604499bd8fc9473a9ec40a913b62d14595
Tobias Bleckert <hola@tobiasbleckert.se>
2020-05-28 13:53:22 +0200

    Merge custom puff block

--- src/blocks/puff.php
+++ src/blocks/puff.php
@@ -2,18 +2,62 @@
 
 function iis_render_puff( $attributes ) {
 	$attributes = array_merge([
+		'custom'       => false,
 		'postId'       => null,
 		'showAsTeaser' => false,
+		'title'        => null,
+		'text'         => null,
+		'imageId'     => null,
 		'alignment'    => null,
+		'url'          => null,
+		'align'        => null,
 	], $attributes);
 
-	$post           = get_post($attributes['postId']);
-	$show_as_teaser = $attributes['showAsTeaser'];
-	$title		    = $post->post_title;
-	$permalink      = get_permalink($post);
-	$media 		    = get_the_terms($post, 'media');
-	$icon 		    = ! is_wp_error( $media ) && $media[0]->name === 'video' ? 'play' : 'arrow-forwards';
-	$class          = 'u-m-b-4';
+	$image_class = ($attributes['showAsTeaser']) ? imns( 'm-teaser__image', false ) : imns( 'm-card__image', false );
+
+	if ($attributes['custom']) {
+		$image = wp_get_attachment_image(
+			$attributes['imageId'],
+			'large',
+			false,
+			[ 'class' => $image_class ]
+		);
+
+		$content = [
+			'thumbnail'  => $image,
+			'title'      => $attributes['title'],
+			'text'       => $attributes['text'],
+			'permalink'  => $attributes['url'],
+			'icon'       => 'arrow-forwards',
+			'date'       => null,
+			'categories' => null,
+			'media'      => null,
+		];
+	} else {
+		$post       = get_post($attributes['postId']);
+		$media      = get_the_terms( $post, 'media' );
+		$icon       = ! is_wp_error( $media ) && $media[0]->name === 'video' ? 'play' : 'arrow-forwards';
+		$date 		= get_the_date( null, $post );
+		$categories = get_the_category( $post );
+		$thumbnail  = get_the_post_thumbnail(
+			$post,
+			'large',
+			[ 'class' => $image_class ]
+		);
+
+		$content    = [
+			'thumbnail'  => $thumbnail,
+			'title'      => $post->post_title,
+			'text'       => $post->post_excerpt,
+			'permalink'  => get_permalink($post),
+			'icon'       => $icon,
+			'date'       => $date,
+			'categories' => $categories,
+			'media'      => $media,
+		];
+	}
+
+	$class = 'u-m-b-4';
 
 	if ( $attributes['align'] === 'right' ) {
 		$class .= ' alignright';
@@ -21,19 +65,16 @@ function iis_render_puff( $attributes ) {
 
 	ob_start();
 
-	if ( $show_as_teaser ) :
-		$thumbnail = get_the_post_thumbnail($post, 'large', [ 'class' => imns( 'm-teaser__image', false ) ]);
-		?>
-
+	if ( $attributes['showAsTeaser'] ) : ?>
 		<div class="<?php echo $class; ?>">
-			<section class="<?php imns( 'm-teaser' ); ?>" id="post-<?php echo $attributes['postId']; ?>">
-				<?php if ( $thumbnail ) echo $thumbnail; ?>
+			<section class="<?php imns( 'm-teaser' ); ?>" id="post-<?php echo $attributes['postId'] ?? 'custom'; ?>">
+				<?php if ( $content['thumbnail'] ) echo $content['thumbnail']; ?>
 				<div class="<?php imns( 'm-teaser__caption' ); ?>">
-					<a class="<?php imns( 'm-teaser__link' ); ?>" href="<?php echo $permalink; ?>">
+					<a class="<?php imns( 'm-teaser__link' ); ?>" href="<?php echo $content['title']; ?>">
 						<h1 class="<?php imns( 'm-teaser__headline' ); ?>">
-							<?php echo $title; ?>
+							<?php echo $content['title']; ?>
 							<svg class="<?php imns( 'icon m-teaser__headline__icon' ); ?>">
-								<use xlink:href="#icon-<?php echo $icon; ?>"></use>
+								<use xlink:href="#icon-<?php echo $content['icon']; ?>"></use>
 							</svg>
 						</h1>
 					</a>
@@ -43,38 +84,36 @@ function iis_render_puff( $attributes ) {
 
 		<?php
 	else :
-		$thumbnail 	= get_the_post_thumbnail($post, 'article-thumb', [ 'class' => imns( 'm-card__image', false ) ]);
-		$excerpt	= $post->post_excerpt;
-		$date 		= get_the_date(null, $post);
-		$categories = get_the_category($post);
 		?>
 
 		<div class="<?php echo $class; ?>">
-			<article class="<?php imns( 'm-card m-card--padded' ); ?>" id="post-<?php echo $attributes['postId']; ?>">
-				<?php if ( $thumbnail ) echo $thumbnail; ?>
+			<article class="<?php imns( 'm-card m-card--padded' ); ?>" id="post-<?php echo $attributes['postId'] ?? 'custom'; ?>">
+				<?php if ( $content['thumbnail'] ) echo $content['thumbnail']; ?>
 				<div class="<?php imns( 'm-card__content' ); ?>">
-					<?php if ( ! is_wp_error( $media ) ) : ?>
+					<?php if ( $content['media'] && ! is_wp_error( $content['media'] ) ) : ?>
 					<div class="<?php imns( 'm-card__meta' ); ?>">
 						<div class="<?php imns( 'a-meta' ); ?>">
-							<?php echo $date; ?>
+							<?php echo $content['date']; ?>
 						</div>
 						<div class="<?php imns( 'a-meta a-meta--ruby' ); ?>">
 							<svg class="<?php imns( 'icon' ); ?>">
-								<use xlink:href="#icon-<?php echo $icon ?>"></use>
-							</svg> <?php echo $media[0]->name; ?>
+								<use xlink:href="#icon-<?php echo $content['icon'] ?>"></use>
+							</svg> <?php echo $content['media'][0]->name; ?>
 						</div>
 					</div>
 					<?php endif; ?>
-					<?php if ( $permalink ) : ?>
-						<a href="<?php echo $permalink; ?>" class="<?php imns( 'm-card__link' ); ?>"><h1 class="beta"><?php echo $title; ?></h1></a>
+					<?php if ( $content['permalink'] ) : ?>
+						<a href="<?php echo $content['permalink']; ?>" class="<?php imns( 'm-card__link' ); ?>">
+							<h1 class="beta"><?php echo $content['title']; ?></h1>
+						</a>
 					<?php else : ?>
-						<h1 class="beta"><?php echo $title; ?></h1>
+						<h1 class="beta"><?php echo $content['title']; ?></h1>
 					<?php endif; ?>
 
-					<?php echo $excerpt; ?>
+					<?php echo $content['text']; ?>
 					<div class="<?php imns( 'm-card__tags' ); ?>">
-						<?php if ( $categories ) : ?>
-							<?php foreach ( $categories as $category ) : ?>
+						<?php if ( $content['categories'] ) : ?>
+							<?php foreach ( $content['categories'] as $category ) : ?>
 								<a href="<?php echo esc_url( get_tag_link( $category->term_id ) ); ?>" class="<?php imns( 'a-tag' ); ?>"><?php echo esc_html( $category->name ); ?></a>
 							<?php endforeach; ?>
 						<?php endif; ?>
--- src/js/blocks/Puff.jsx
+++ src/js/blocks/Puff.jsx
@@ -1,7 +1,20 @@
 const { __ } = wp.i18n;
+const { Fragment } = wp.element;
 const { useEffect, useState } = wp.element;
 const { registerBlockType } = wp.blocks;
-const { SelectControl, CheckboxControl } = wp.components;
+const {
+	SelectControl,
+	CheckboxControl,
+	PanelBody,
+	Button,
+	TextControl,
+} = wp.components;
+const {
+	InspectorControls,
+	MediaUpload,
+	MediaUploadCheck,
+	RichText,
+} = wp.editor;
 
 registerBlockType('iis/puff', {
 	title: __('Post puff', 'iis'),
@@ -16,26 +29,100 @@ registerBlockType('iis/puff', {
 		align: ['right'],
 	},
 	attributes: {
+		custom: {
+			type: 'boolean',
+			default: false,
+		},
 		postId: {
 			type: 'string',
 			default: null,
 		},
+		postPreview: {
+			type: 'object',
+			default: {},
+		},
 		showAsTeaser: {
 			type: 'boolean',
-			default: false
+			default: false,
+		},
+		imagePreviewUrl: {
+			type: 'string',
+			default: null,
+		},
+		imageId: {
+			type: 'number',
+			default: null,
+		},
+		title: {
+			type: 'string',
+			default: '',
+		},
+		text: {
+			type: 'string',
+			default: '',
+		},
+		url: {
+			type: 'text',
+			default: null,
 		},
 	},
 	edit({ attributes, setAttributes }) {
 		const [posts, setPosts] = useState([]);
-		const containerStyles = {
-			border: '1px dotted #bbb',
+		const {
+			showAsTeaser,
+			custom,
+		} = attributes;
+		const styleCard = {
+			position: 'relative',
+			border: '1px solid #bbb',
+			borderRadius: '.25rem',
+			overflow: 'hidden',
+			maxWidth: '400px',
+		};
+
+		const styleCardContent = {
 			padding: '1rem',
 		};
 
+		const styleCardImage = {
+			width: '100%',
+			display: 'block',
+			borderRadius: '.25rem .25rem 0 0',
+		};
+
+		const styleTeaserImage = {
+			width: '100%',
+			height: '100%',
+			minHeight: '300px',
+			display: 'block',
+			objectFit: 'cover',
+		};
+
+		const styleTeaserContent = {
+			display: 'flex',
+			flexDirection: 'column',
+			alignContent: 'stretch',
+			justifyContent: 'flex-end',
+			position: 'absolute',
+			right: 0,
+			bottom: 0,
+			left: 0,
+			textShadow: '0 0 1rem rgba(0,0,0,.5)',
+			backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,0) 0,transparent 55%,#000)',
+			padding: '2rem',
+			color: '#fff',
+		};
+
+		let image = null;
+
+		if (attributes.imagePreviewUrl) {
+			image = <img src={attributes.imagePreviewUrl} alt="" style={{ width: '100%', height: 'auto' }} />;
+		}
+
 		useEffect(() => {
-			wp.apiFetch({ path: '/wp/v2/posts?parent=0&per_page=-1' }).then((posts) => {
+			wp.apiFetch({ path: '/wp/v2/posts?parent=0&per_page=-1' }).then((fetchedPosts) => {
 				// Make posts in options format.
-				const postOptions = posts.map((post) => ({
+				const postOptions = fetchedPosts.map((post) => ({
 					label: post.title.rendered,
 					value: post.id,
 				}));
@@ -45,37 +132,119 @@ registerBlockType('iis/puff', {
 		}, []);
 
 		return (
-			<div style={containerStyles}>
-				<fieldset>
-					<legend>{__('Post puff', 'iis')}</legend>
-
-					<SelectControl
-						label={__('Post', 'iis')}
-						onChange={(postId) => setAttributes({ postId })}
-						options={posts.length ? (
-							[
-								{
-									label: __('Choose a post', 'internetkunskap'),
-									value: null,
-								},
-								...posts,
-							]
-						) : [
-							{
-								label: __('Loading posts...', 'internetkunskap'),
-								value: '',
-								disabled: true,
-							},
-						]}
-						value={attributes.postId}
-					/>
-					<CheckboxControl
-						label="Show as teaser"
-						checked={attributes.showAsTeaser}
-						onChange={(showAsTeaser) => setAttributes({ showAsTeaser })}
-					/>
-				</fieldset>
-			</div>
+			<Fragment>
+				<InspectorControls>
+					<PanelBody title="Content">
+						<CheckboxControl
+							label="Custom content"
+							checked={custom}
+							onChange={(value) => setAttributes({ custom: value })}
+						/>
+						{custom && (
+							<TextControl
+								label={__('Link', 'iis')}
+								placeholder={__('/link/to/here', 'iis')}
+								value={attributes.url}
+								onChange={(url) => setAttributes({ url })}
+							/>
+						)}
+					</PanelBody>
+					<PanelBody title="Display">
+						<CheckboxControl
+							label="Show as teaser"
+							checked={showAsTeaser}
+							onChange={(value) => setAttributes({ showAsTeaser: value })}
+						/>
+					</PanelBody>
+					{attributes.custom && (
+						<PanelBody title="Image">
+							<div>{image}</div>
+							{attributes.imagePreviewUrl === null && (
+								<MediaUploadCheck>
+									<MediaUpload
+										onSelect={(imageObject) => (
+											setAttributes({
+												imagePreviewUrl: imageObject.sizes.full.url,
+												imageId: imageObject.id,
+											})
+										)}
+										type="image"
+										value={attributes.imagePreviewUrl}
+										render={({ open }) => (
+											<Button
+												className="components-button editor-post-featured-image__toggle"
+												onClick={open}
+											>
+												Upload Image!
+											</Button>
+										)}
+									/>
+								</MediaUploadCheck>
+							)}
+							{attributes.imagePreviewUrl !== null && (
+								<Button
+									className="components-button is-button is-default"
+									onClick={() => setAttributes({ imagePreviewUrl: null, imageId: null })}
+								>
+									Remove image
+								</Button>
+							)}
+						</PanelBody>
+					)}
+				</InspectorControls>
+				<div style={styleCard}>
+					{custom && attributes.imagePreviewUrl && (
+						<img
+							src={attributes.imagePreviewUrl}
+							alt=""
+							style={(showAsTeaser) ? styleTeaserImage : styleCardImage}
+						/>
+					)}
+					<div
+						style={(showAsTeaser && custom) ? styleTeaserContent : styleCardContent}
+					>
+						{!custom && (
+							<SelectControl
+								label={__('Post', 'iis')}
+								onChange={(postId) => setAttributes({ postId })}
+								options={posts.length ? (
+									[
+										{
+											label: __('Choose a post', 'internetkunskap'),
+											value: null,
+										},
+										...posts,
+									]
+								) : [
+									{
+										label: __('Loading posts...', 'internetkunskap'),
+										value: '',
+										disabled: true,
+									},
+								]}
+								value={attributes.postId}
+							/>
+						)}
+						{custom && (
+							<div>
+								<RichText
+									tagName="h1"
+									value={attributes.title}
+									placeholder={__('Title')}
+									style={{ margin: 0 }}
+									onChange={(title) => setAttributes({ title })}
+								/>
+								<RichText
+									tagName="div"
+									value={attributes.text}
+									placeholder={__('Content')}
+									onChange={(text) => setAttributes({ text })}
+								/>
+							</div>
+						)}
+					</div>
+				</div>
+			</Fragment>
 		);
 	},
 	save() {
