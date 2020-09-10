98f2c4b22e15170d2f0a1ca90936f205ffb33260
Tobias Bleckert <hola@tobiasbleckert.se>
2020-09-10 14:17:00 +0200

    Bump version

--- blocks.php
+++ blocks.php
@@ -5,7 +5,7 @@
  * Description: Block library from Internetstiftelsen
  * Author: Internetstiftelsen
  * Author URI: https://internetstiftelsen.se
- * Version: 4.2.4
+ * Version: 4.5.0
  * Plugin URI: https://github.com/sewebb/iis-blocks
  * Text Domain: iis-blocks
  * Domain Path: /languages/
