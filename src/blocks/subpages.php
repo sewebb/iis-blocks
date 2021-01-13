acb19007158ab8a3cd00dcbe243dbad3ecd6ba0e
Tobias Bleckert <hola@tobiasbleckert.se>
2021-01-13 10:01:53 +0100

    Wrap alignment

--- src/blocks/subpages.php
+++ src/blocks/subpages.php
@@ -9,10 +9,7 @@ function iis_render_block_subpages( $attributes, $content ) {
 	);
 
 	$class_name = 'm-submenu';
-
-	if ( $attributes['align'] ) {
-		$class_name .= ' !align' . $attributes['align'];
-	}
+	$alignment  = 'align' . $attributes['align'];
 
 	global $post;
 
@@ -49,65 +46,67 @@ function iis_render_block_subpages( $attributes, $content ) {
 
 	ob_start();
 	?>
-	<dl class="<?php imns( $class_name ); ?>">
-		<dt class="<?php imns( 'm-submenu__title' ); ?>">
-			<a href="<?php echo get_permalink( $top_level ); ?>" class="<?php imns( 'm-submenu__title__link' ); ?>">
-				<span><?php echo apply_filters( 'the_title', $top_level->post_title ); ?></span>
-				<svg class="icon">
-					<use xlink:href="#icon-arrow-variant"></use>
-				</svg>
-			</a>
-		</dt>
-		<?php
+	<div class="<?php echo $alignment; ?>">
+		<dl class="<?php imns( $class_name ); ?>">
+			<dt class="<?php imns( 'm-submenu__title' ); ?>">
+				<a href="<?php echo get_permalink( $top_level ); ?>" class="<?php imns( 'm-submenu__title__link' ); ?>">
+					<span><?php echo apply_filters( 'the_title', $top_level->post_title ); ?></span>
+					<svg class="icon">
+						<use xlink:href="#icon-arrow-variant"></use>
+					</svg>
+				</a>
+			</dt>
+			<?php
 
-		foreach ( $children as $child ) :
-			$link_classes = 'm-submenu__item__link';
+			foreach ( $children as $child ) :
+				$link_classes = 'm-submenu__item__link';
 
-			if ( count( $child['children'] ) ) {
-				$link_classes .= ' m-submenu__item__link--has-sublevel';
-			}
+				if ( count( $child['children'] ) ) {
+					$link_classes .= ' m-submenu__item__link--has-sublevel';
+				}
 
-			if ( $child['post']->ID === $post->ID ) {
-				$link_classes .= ' !is-current';
-			}
+				if ( $child['post']->ID === $post->ID ) {
+					$link_classes .= ' !is-current';
+				}
 
-			?>
-			<dd class="<?php imns( 'm-submenu__item' ); ?>">
-				<?php if ( count( $child['children'] ) ) : ?>
-					<div class="<?php imns( 'm-submenu__item__sublevel' ); ?>">
+				?>
+				<dd class="<?php imns( 'm-submenu__item' ); ?>">
+					<?php if ( count( $child['children'] ) ) : ?>
+						<div class="<?php imns( 'm-submenu__item__sublevel' ); ?>">
+							<a href="<?php echo get_permalink( $child['post'] ); ?>" class="<?php imns( $link_classes ); ?>">
+								<span><?php echo apply_filters( 'the_title', $child['post']->post_title ); ?></span>
+								<svg class="icon">
+									<use xlink:href="#icon-arrow-variant"></use>
+								</svg>
+							</a>
+							<button type="button" class="<?php imns( 'm-submenu__item__toggle-button' ); ?>" data-a11y-toggle="sublvl<?php echo $child['post']->ID; ?>" aria-controls="sublvl<?php echo $child['post']->ID; ?>">
+								<span class="u-visuallyhidden">Öppna/stäng</span>
+							</button>
+						</div>
+						<ul class="<?php imns( 'm-submenu__sublevel' ); ?>" aria-hidden="true" id="sublvl<?php echo $child['post']->ID; ?>" data-focus-trap="false">
+							<?php foreach ( $child['children'] as $subchild ) : ?>
+								<li class="<?php imns( 'm-submenu__sublevel__item' ); ?>">
+									<a href="<?php echo get_permalink( $subchild ); ?>" class="<?php imns( 'm-submenu__item__link m-submenu__sublevel__item__link' ); ?>">
+										<span><?php echo apply_filters( 'the_title', $subchild->post_title ); ?></span>
+										<svg class="icon">
+											<use xlink:href="#icon-arrow-variant"></use>
+										</svg>
+									</a>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					<?php else : ?>
 						<a href="<?php echo get_permalink( $child['post'] ); ?>" class="<?php imns( $link_classes ); ?>">
 							<span><?php echo apply_filters( 'the_title', $child['post']->post_title ); ?></span>
 							<svg class="icon">
 								<use xlink:href="#icon-arrow-variant"></use>
 							</svg>
 						</a>
-						<button type="button" class="<?php imns( 'm-submenu__item__toggle-button' ); ?>" data-a11y-toggle="sublvl<?php echo $child['post']->ID; ?>" aria-controls="sublvl<?php echo $child['post']->ID; ?>">
-							<span class="u-visuallyhidden">Öppna/stäng</span>
-						</button>
-					</div>
-					<ul class="<?php imns( 'm-submenu__sublevel' ); ?>" aria-hidden="true" id="sublvl<?php echo $child['post']->ID; ?>" data-focus-trap="false">
-						<?php foreach ( $child['children'] as $subchild ) : ?>
-						<li class="<?php imns( 'm-submenu__sublevel__item' ); ?>">
-							<a href="<?php echo get_permalink( $subchild ); ?>" class="<?php imns( 'm-submenu__item__link m-submenu__sublevel__item__link' ); ?>">
-								<span><?php echo apply_filters( 'the_title', $subchild->post_title ); ?></span>
-								<svg class="icon">
-									<use xlink:href="#icon-arrow-variant"></use>
-								</svg>
-							</a>
-						</li>
-						<?php endforeach; ?>
-					</ul>
-				<?php else : ?>
-					<a href="<?php echo get_permalink( $child['post'] ); ?>" class="<?php imns( $link_classes ); ?>">
-						<span><?php echo apply_filters( 'the_title', $child['post']->post_title ); ?></span>
-						<svg class="icon">
-							<use xlink:href="#icon-arrow-variant"></use>
-						</svg>
-					</a>
-				<?php endif; ?>
-			</dd>
-		<?php endforeach; ?>
-	</dl>
+					<?php endif; ?>
+				</dd>
+			<?php endforeach; ?>
+		</dl>
+	</div>
 	<?php
 
 	return ob_get_clean();
