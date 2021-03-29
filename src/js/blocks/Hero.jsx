f28f7a1bc88d4598eff21d8e5474eae864c34d4d
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2021-03-29 13:58:52 +0200

    Bugfix

--- src/js/blocks/Hero.jsx
+++ src/js/blocks/Hero.jsx
@@ -13,6 +13,10 @@ const {
 } = wp.editor;
 
 function parseYoutube(url) {
+	if (!url) {
+		return null;
+	}
+
 	const regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
 	const match = url.match(regExp);
 	return (match && match[7].length === 11) ? match[7] : false;
