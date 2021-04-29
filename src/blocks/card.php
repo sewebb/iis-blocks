fe0e139a8e5c94e6f70734a96c52ed2b4e1aa2d3
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2021-04-29 09:44:42 +0200

    Display image

--- src/blocks/card.php
+++ src/blocks/card.php
@@ -27,10 +27,30 @@ function iis_render_card( $attributes, $content ) {
 		$class .= ' m-card--shadow';
 	}
 
+	$image = null;
+
+	if ( $attributes['imageId'] ) {
+		$image_class = imns( 'm-card__image m-card__media', false );
+		$image_size  = ( $attributes['showAsTeaser'] ) ? 'puff-teaser-image' : 'puff-image';
+		$image_sizes = apply_filters( 'iis_blocks_puff_image_sizes', [ 'puff-image', 'puff-teaser-image', 'puff-image-4:3' ] );
+
+		if ( null !== $attributes['imageSize'] && in_array( $attributes['imageSize'], $image_sizes, true ) ) {
+			$image_size = $attributes['imageSize'];
+		}
+
+		$image = wp_get_attachment_image(
+			$attributes['imageId'],
+			$image_size,
+			false,
+			[ 'class' => $image_class ]
+		);
+	}
+
 	ob_start();
 	?>
 	<div class="align<?php echo esc_attr( $attributes['align'] ); ?>">
 		<article class="wp-block-iis-card <?php imns( $class ); ?>">
+			<?php echo $image; ?>
 			<div class="<?php imns( 'm-card__content' ); ?>">
 				<?php echo $content; ?>
 			</div>
