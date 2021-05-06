389e0e200811ab286f3b828af2c3c9acfca00a6c
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2021-05-06 10:28:34 +0200

    Migrated to laravel-mix 6

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,4 @@ const mix = require('laravel-mix');
 
 mix
 	.js('./src/js/blocks.js', './dist/js/blocks.js')
-	.sass('./src/scss/blocks.scss', './dist/css/blocks.css', {
-		includePaths: ['node_modules'],
-	});
+	.sass('./src/scss/blocks.scss', './dist/css/blocks.css');
