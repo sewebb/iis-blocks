a1b17ec3778a584e0a8e23c2c0f2621f1a45f37c
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2021-08-04 16:09:36 +0200

    Open post puff in new tab

--- src/blocks/puff.php
+++ src/blocks/puff.php
@@ -26,6 +26,7 @@ function iis_render_puff( $attributes, $inner_content ) {
 			'className'      => '',
 			'button'         => false,
 			'shadow'         => false,
+			'target'         => '_self',
 		],
 		$attributes
 	);
@@ -151,7 +152,7 @@ function iis_render_puff( $attributes, $inner_content ) {
 				</time>
 			<?php endif; ?>
 			<?php if ( $content['permalink'] ) : ?>
-				<a href="<?php echo $content['permalink']; ?>" class="<?php imns( 'm-card__link' ); ?>">
+				<a href="<?php echo $content['permalink']; ?>" class="<?php imns( 'm-card__link' ); ?>" target="<?php echo esc_attr( $attributes['target'] ); ?>">
 					<h2 class="<?php echo $headline_size; ?>">
 						<?php echo $content['title']; ?>
 
--- src/js/blocks/Puff.jsx
+++ src/js/blocks/Puff.jsx
@@ -94,6 +94,10 @@ registerBlockType('iis/puff', {
 			type: 'boolean',
 			default: false,
 		},
+		target: {
+			type: 'string',
+			default: '_self',
+		},
 	},
 	edit({ attributes, setAttributes }) {
 		const [imageSizes, setImageSizes] = useState(null);
@@ -224,6 +228,11 @@ registerBlockType('iis/puff', {
 						/>
 					</PanelBody>
 					<PanelBody title="Display">
+						<CheckboxControl
+							label="Open link in new tab"
+							checked={attributes.target === '_blank'}
+							onChange={(value) => setAttributes({ target: (value) ? '_blank' : '_self' })}
+						/>
 						<CheckboxControl
 							label="Show as teaser"
 							checked={showAsTeaser}
