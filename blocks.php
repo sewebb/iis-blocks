033cafdc5d4ffdd4528f2475ed61888bf8c00d5a
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2022-01-17 14:39:30 +0100

    Allow iis-library 3.0

--- blocks.php
+++ blocks.php
@@ -5,7 +5,7 @@
  * Description: Block library from Internetstiftelsen
  * Author: Internetstiftelsen
  * Author URI: https://internetstiftelsen.se
- * Version: 6.2.4
+ * Version: 6.2.5
  * Plugin URI: https://github.com/sewebb/iis-blocks
  * Text Domain: iis-blocks
  * Domain Path: /languages/
