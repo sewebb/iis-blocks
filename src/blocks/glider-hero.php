3951e5a1d2e6fecd21c94b63a61ff4de44a4f07a
Robert Hedman <robert.hedman@internetstiftelsen.se>
2022-05-03 14:29:54 +0200

    added missing CSS-class

--- src/blocks/glider-hero.php
+++ src/blocks/glider-hero.php
@@ -12,7 +12,7 @@ function iis_render_glider_hero( $attributes, $content ) {
 	?>
 	<div class="wp-block-iis-glider-hero alignfull <?php echo iis_sanitize_html_classes( $attributes['className'] ); ?>">
 		<div class="glider-hero-contain u-p-x-0">
-			<button type="button" class="<?php imns( 'a-paging' ); ?> glider-nav glider-nav--hero js-glider-prev">
+			<button type="button" class="<?php imns( 'a-paging' ); ?> glider-nav glider-nav--hero glider-prev js-glider-prev">
 				<span class="u-visuallyhidden"><?php _e( 'Backward', 'iis-blocks' ); ?></span>
 				<svg class="icon <?php imns( 'a-paging__icon' ); ?>">
 					<use xlink:href="#icon-arrow-backwards"></use>
