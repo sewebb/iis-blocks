0841723e44f29fc469a8bfee29ac4eed74b92d83
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2022-09-21 10:57:43 +0200

    Empty string of no content

--- src/blocks/download.php
+++ src/blocks/download.php
@@ -18,7 +18,7 @@ function iis_render_block_download( $attributes ) {
 						</a>
 					</div>
 					<div class="grid-18  display-flex flex-column flex-sm-row justify-content-between align-items-start">
-						<p class="u-m-b-0"><?php echo $attributes['content']; ?></p>
+						<p class="u-m-b-0"><?php echo $attributes['content'] ?? ''; ?></p>
 						<div class="display-flex align-self-end flex-nowrap <?php imns( 'm-download__trigger' ); ?>">
 							<strong class="<?php imns( 'm-download__text' ); ?>">
 								<?php echo esc_html( apply_filters( 'iis_blocks_download_btn', __( 'Download', 'iis-blocks' ) ) ); ?>
