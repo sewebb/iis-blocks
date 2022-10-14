09cac58f95935721aa9a86c21d49c4f51585c3c2
Robert Hedman <robert.hedman@internetstiftelsen.se>
2022-10-14 09:14:41 +0200

    change version number

--- blocks.php
+++ blocks.php
@@ -5,7 +5,7 @@
  * Description: Block library from Internetstiftelsen
  * Author: Internetstiftelsen
  * Author URI: https://internetstiftelsen.se
- * Version: 6.3.6
+ * Version: 6.3.7
  * Plugin URI: https://github.com/sewebb/iis-blocks
  * Text Domain: iis-blocks
  * Domain Path: /languages/
