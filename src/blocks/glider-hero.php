0dabdd2759a81cfb22c73da5742934371dc8a423
Robert Hedman <robert.hedman@internetstiftelsen.se>
2023-01-09 14:20:34 +0100

    readspeaker skip buttons

--- src/blocks/glider-hero.php
+++ src/blocks/glider-hero.php
@@ -13,7 +13,7 @@ function iis_render_glider_hero( $attributes, $content ) {
 	?>
 	<div class="wp-block-iis-glider-hero alignfull <?php echo iis_sanitize_html_classes( $attributes['className'] ); ?>">
 		<div class="glider-hero-contain u-p-x-0">
-			<button type="button" class="<?php imns( 'a-paging' ); ?> glider-nav glider-nav--hero glider-prev js-glider-prev">
+			<button type="button" class="<?php imns( 'a-paging' ); ?> glider-nav glider-nav--hero glider-prev js-glider-prev rs_skip">
 				<span class="u-visuallyhidden"><?php _e( 'Backward', 'iis-blocks' ); ?></span>
 				<svg class="icon <?php imns( 'a-paging__icon' ); ?>">
 					<use xlink:href="#icon-arrow-backwards"></use>
@@ -22,7 +22,7 @@ function iis_render_glider_hero( $attributes, $content ) {
 			<div class="glider js-glider-hero" <?php echo ( $attributes['autoScrollTime'] ) ? 'data-timeout="' . esc_attr( $attributes['autoScrollTime'] ) . '"' : ''; ?>>
 				<?php echo $content; ?>
 			</div>
-			<button type="button" class="<?php imns( 'a-paging' ); ?> glider-nav glider-nav--hero glider-next js-glider-next">
+			<button type="button" class="<?php imns( 'a-paging' ); ?> glider-nav glider-nav--hero glider-next js-glider-next rs_skip">
 				<span class="u-visuallyhidden"><?php _e( 'Forward', 'iis-blocks' ); ?></span>
 				<svg class="icon <?php imns( 'a-paging__icon' ); ?>">
 					<use xlink:href="#icon-arrow-forwards"></use>
