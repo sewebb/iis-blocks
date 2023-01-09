116f9860e1209cc73cdc6924cec59401342757a6
Robert Hedman <robert.hedman@internetstiftelsen.se>
2023-01-09 14:24:30 +0100

    bump version

--- blocks.php
+++ blocks.php
@@ -5,7 +5,7 @@
  * Description: Block library from Internetstiftelsen
  * Author: Internetstiftelsen
  * Author URI: https://internetstiftelsen.se
- * Version: 6.3.8
+ * Version: 6.3.10
  * Plugin URI: https://github.com/sewebb/iis-blocks
  * Text Domain: iis-blocks
  * Domain Path: /languages/
