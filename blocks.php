56828dc4e7118b8b9126c3db9ef018d1989be716
Richard Brattlund <info@richardbrattlund.se>
2023-05-03 14:36:36 +0200

    Bump version

--- blocks.php
+++ blocks.php
@@ -5,7 +5,7 @@
  * Description: Block library from Internetstiftelsen
  * Author: Internetstiftelsen
  * Author URI: https://internetstiftelsen.se
- * Version: 7.0.5
+ * Version: 7.0.6
  * Plugin URI: https://github.com/sewebb/iis-blocks
  * Text Domain: iis-blocks
  * Domain Path: /languages/
