f9c4712bc89458ebf94ff7641fde153f47a5c775
Robert Hedman <robert.hedman@internetstiftelsen.se>
2025-06-02 13:57:04 +0200

    tag new version

--- blocks.php
+++ blocks.php
@@ -5,7 +5,7 @@
  * Description: Block library from Internetstiftelsen
  * Author: Internetstiftelsen
  * Author URI: https://internetstiftelsen.se
- * Version: 7.0.27
+ * Version: 7.0.28
  * Plugin URI: https://github.com/sewebb/iis-blocks
  * Text Domain: iis-blocks
  * Domain Path: /languages/
