fdae9b4e64f48052371b69e7e6f8c13acd6979d9
Tobias Bleckert <tobias.bleckert@internetstiftelsen.se>
2025-10-08 16:12:45 +0200

    Render items

--- src/blocks/selectable-item.php
+++ src/blocks/selectable-item.php
@@ -4,15 +4,22 @@ function iis_render_selectable_item( $attributes, $content ) {
 	$attributes = array_merge(
 		[
 			'className'  => '',
+			'title'      => '',
 		],
 		$attributes,
 	);
 
+	if ( ! $attributes['title'] ) {
+		return null;
+	}
+
 	$class = $attributes['className'];
+	$slug = sanitize_title( $attributes['title'] );
 
 	ob_start();
 	?>
-	<div class="wp-block-iis-selectable-item <?php echo iis_sanitize_html_classes( $class ); ?>">
+	<div class="wp-block-iis-selectable-item <?php echo iis_sanitize_html_classes( $class ); ?>" id="<?php echo esc_attr( $slug ); ?>" data-selectable-item>
+		<h2><?php echo esc_html( $attributes['title'] ); ?></h2>
 		<?php echo $content; ?>
 	</div>
 	<?php
